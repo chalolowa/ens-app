7fb03825531f5715c78a9535cf34472f1895067a
Chalolowa <locha.softwaredev@gmail.com>
2024-10-16 08:12:14 +0300

    On branch main  Changes to be committed: 	new file:   next.config.js 	deleted:    next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   public/learnweb3punks.png 	modified:   src/app/layout.js 	modified:   src/app/page.js 	new file:   src/app/providers.js

---
+++ src/app/providers.js
@@ -0,0 +1,54 @@
+"use client";
+
+import * as React from "react";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
+//importing the chains we need (here, just Sepolia)
+import {
+  sepolia
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const { wallets } = getDefaultWallets();
+
+export const config = getDefaultConfig({
+  appName: "ENS dapp",
+  projectId: "c39e139caa1794829cd56b35bf01d5c8",
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+   sepolia
+  ],
+  ssr: true,
+});
+
+// TanStack Query is a library that makes it very easy to fetch, cache and handle data.
+// It gives you declarative, always-up-to-date auto-managed queries and mutations.
+export const queryClient = new QueryClient();
+
+export function Providers({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
